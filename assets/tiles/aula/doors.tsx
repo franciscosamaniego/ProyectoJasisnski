30571ab3bf883b29a9093781ffbe5b9d382f0d4a
franciscosamaniego <102254119+franciscosamaniego@users.noreply.github.com>
2024-08-04 16:26:49 -0300

    Delete assets directory

--- assets/tiles/aula/doors.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="doors" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../../doors.png" width="260" height="280"/>
-</tileset>
